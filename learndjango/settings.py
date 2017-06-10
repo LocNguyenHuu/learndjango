f64168215f06b475578d2faec993778d0b159caa
LocNguyenHuu <thelionoflaziness@gmail.com>
2017-06-10 15:55:31 +0700

    Add bootstrap library

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIR = (
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
